59bf2c2be2af61cff159928415b3d8baab2dd379
suchit41 <suchitpravinjain@gmail.com>
2024-03-23 03:17:50 +0530

    Header Updated

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import MenuItems from './MenuItems'
+import {AiFillHome} from 'react-icons/ai'
+import {BsFillInfoCircleFill} from 'react-icons/bs'
+import { Span } from 'next/dist/trace'
+
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+        <div className='flex gap-4'>
+        <MenuItems title="Home" address="/" Icon={AiFillHome} />
+        <MenuItems title="About" address="/about" Icon={BsFillInfoCircleFill} />
+
+        </div>
+        <div className='flex gap-1 items-center'>
+            <span className='text-2xl font-bold py-1 px-2 bg-amber-500 rounded-lg'>IMDB</span>
+            <span className='text-xl hidden sm:inline'>Clone </span>
+
+        </div>
+    </div>
+  )
+}
